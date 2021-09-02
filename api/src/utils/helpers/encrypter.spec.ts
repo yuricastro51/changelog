cf0cfcab5befe5dd6bc9224b2e0b516e01459d75
Yuri Castro <yuricastro51@gmail.com>
2021-09-01 21:16:29 -0300

    ensure Encrypter throws if no params are provided

--- api/src/utils/helpers/encrypter.spec.ts
+++ api/src/utils/helpers/encrypter.spec.ts
@@ -13,6 +13,7 @@ class BcryptTest {
 jest.mock('bcrypt', () => new BcryptTest());
 
 import bcrypt from 'bcrypt';
+import MissingParamError from '../errors/missingParamError';
 
 import { Encrypter } from './encrypter';
 
@@ -39,12 +40,21 @@ describe('Encrypter', () => {
 		expect(isValid).toBe(false);
 	});
 
-	test('Should call Encrypter with correct values', async () => {
+	test('Should call bcrypt with correct values', async () => {
 		const { sut } = makeSut();
 		await sut.compare('any_value', 'hashed_value');
 
 		//@ts-ignore
 		expect(bcrypt.value).toBe('any_value');
 		expect(bcrypt.hash).toBe('hashed_value');
+	});
+
+	test('Should throw if no params are provided', async () => {
+		const { sut } = makeSut();
+		const promiseWithoutValue = sut.compare('', 'hashed_value');
+		const promiseWithoutHasedValue = sut.compare('any_value', '');
+
+		await expect(promiseWithoutValue).rejects.toThrow(new MissingParamError('value'));
+		await expect(promiseWithoutHasedValue).rejects.toThrow(new MissingParamError('hash'));
 	});
 });
--- api/src/utils/helpers/encrypter.ts
+++ api/src/utils/helpers/encrypter.ts
@@ -1,8 +1,15 @@
 import bcrypt from 'bcrypt';
 import { IEncrypter } from 'src/interfaces/encrypter';
+import MissingParamError from '../errors/missingParamError';
 
 export class Encrypter implements IEncrypter {
 	async compare(value: string, hash: string): Promise<boolean> {
+		if (!value) {
+			throw new MissingParamError('value');
+		}
+		if (!hash) {
+			throw new MissingParamError('hash');
+		}
 		const isValid = await bcrypt.compare(value, hash);
 		return isValid;
 	}
