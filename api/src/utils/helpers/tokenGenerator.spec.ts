a08df2424f48266e5d1456f14026a5c8f9fa697d
Yuri Castro <yuricastro51@gmail.com>
2021-09-02 20:06:14 -0300

    ensure TokenGenerator returns a token if jwt returns token

--- api/src/utils/helpers/tokenGenerator.spec.ts
+++ api/src/utils/helpers/tokenGenerator.spec.ts
@@ -1,3 +1,14 @@
+class JwtTest {
+	token = 'any_token';
+
+	sign(payload: string, secret: string): string | null {
+		return this.token;
+	}
+}
+
+jest.mock('jsonwebtoken', () => new JwtTest());
+
+import jwt from 'jsonwebtoken';
 import { TokenGenerator } from './tokenGenerator';
 
 const makeSut = () => {
@@ -9,8 +20,18 @@ const makeSut = () => {
 describe('TokenGenerator', () => {
 	test('Should return null if jwt returns null', async () => {
 		const { sut } = makeSut();
+		//@ts-ignore
+		jwt.token = null;
 
 		const token = await sut.generate('any_id');
 		expect(token).toBeNull();
+	});
+
+	test('Should return a token if jwt returns token', async () => {
+		const { sut } = makeSut();
+		const token = await sut.generate('any_id');
+
+		//@ts-ignore
+		expect(token).toBe(jwt.token);
 	});
 });
--- api/src/utils/helpers/tokenGenerator.ts
+++ api/src/utils/helpers/tokenGenerator.ts
@@ -1,7 +1,9 @@
+import jwt from 'jsonwebtoken';
 import { ITokenGenerator } from 'src/interfaces/tokenGenerator';
 
 export class TokenGenerator implements ITokenGenerator {
 	async generate(userId: string): Promise<string | null> {
-		return null;
+		const token = jwt.sign(userId, 'secret');
+		return token;
 	}
 }
