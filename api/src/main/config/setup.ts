b654bed84b14cf6b2408918be9717c1905fdd305
Yuri Castro <yuricastro51@gmail.com>
2021-09-16 19:18:34 -0300

    enable CORS

--- api/src/main/config/setup.ts
+++ api/src/main/config/setup.ts
@@ -2,4 +2,10 @@ import { Express } from 'express';
 
 export function setupApp(app: Express) {
 	app.disable('x-powered-by');
+	app.use((req, res, next) => {
+		res.set('access-control-allow-origin', '*');
+		res.set('access-control-allow-method', '*');
+		res.set('access-control-allow-headers', '*');
+		next();
+	});
 }
